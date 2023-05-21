7882b046fe726a3ec5061fa6410d645286d21205
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-05-21 11:27:48 -0400

    basic functionality added to slider

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const gridcontainer = document.querySelector('.grid')
+window.onload = function() {
+    setGrid(16);
+  };
 
 
-for(let i=0; i<256; i++){
-    const griditem = document.createElement('div')
-    griditem.className = 'grid-item' 
-    gridcontainer.appendChild(griditem)
+function setGrid(size){
+    const gridcontainer = document.querySelector('.grid')
+    document.getElementsByClassName('grid')[0].textContent = '';
+    for(let i=0; i<(size*size); i++){
+        const griditem = document.createElement('div')
+        griditem.className = 'grid-item' 
+        gridcontainer.appendChild(griditem)
+    }
 }
+
 
 let blockClick = document.body.addEventListener('mouseover', function(e){
     if(e.target.className == "grid-item"){
-        console.log(e)
         e.target.style.backgroundColor = 'black';
     }
 })
 
-range.addEventListener("input", function(e){
-    console.log(e)
+let sliderChange = document.body.addEventListener("input", function(e){
+    let chosenSize = document.getElementById("slider").value
+    document.querySelector('.reader').innerHTML = chosenSize
+    setGrid(chosenSize)
 })
