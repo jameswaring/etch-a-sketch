21c1e3bc62b1e025966c6bec26903819f88df55f
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-05-21 10:40:26 -0400

    changed event listener to mouse over

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for(let i=0; i<256; i++){
     gridcontainer.appendChild(griditem)
 }
 
-let blockClick = document.body.addEventListener('click', function(e){
+let blockClick = document.body.addEventListener('mouseover', function(e){
     if(e.target.className == "grid-item"){
         console.log(e)
         e.target.style.backgroundColor = 'black';
